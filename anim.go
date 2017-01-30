803c32a9cb2f13baf62c0fb561af26f7cc0dd3f1
Daniel Skinner <daniel@dasa.cc>
2017-01-30 08:57:23 -0600

    anim: update api call
    Fixes #12

--- anim.go
+++ anim.go
@@ -56,7 +56,7 @@ func (anim Animation) Do() (quit chan struct{}) {
 					quit <- struct{}{}
 					t = 1
 				}
-				dt := float32(anim.Sig.SampleUnit(t))
+				dt := float32(anim.Sig.Index(t))
 				if anim.Interp != nil {
 					anim.Interp(dt)
 				}
@@ -84,7 +84,7 @@ func Animate(mat *f32.Mat4, interp Interpolator, fn func(m *f32.Mat4, dt float32
 					quit <- struct{}{}
 					t = 1
 				}
-				dt := float32(interp.Sig.SampleUnit(t))
+				dt := float32(interp.Sig.Index(t))
 				fn(&m, dt)
 			}
 		}
